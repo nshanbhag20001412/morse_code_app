698f89be60682eeb14d9c3d46f643f062ac874d2
nshanbhag20001412 <nshanbhag1412@gmail.com>
2021-07-31 11:24:33 +0530

    updated js file with server for translation

--- app.js
+++ app.js
@@ -1,72 +1,42 @@
-// function translate()
-// {
-//     if((ch >= 97 && ch <= 122) || (ch >= 65 && ch <= 90))
-//         encode();
-//     else
-//         decode();
-// }
+var btnTranslate_english = document.querySelector("#btn-translate-english");
+// var btnTranslate_morse = document.querySelector("#btn-translate-morse");
+// var btnTranslate_audio = document.querySelector("#btn-translate-audio");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
+var serverURLtranseng = "https://api.funtranslations.com/translate/morse.json"
+// var serverURLtransmorse = "http://api.funtranslations.com/translate/morse2english.json"
+// var serverURLa = "http://api.funtranslations.com/translate/morse2english.json"
 
-function encode() 
-{
-    document.morsecode.encodei.value=document.morsecode.encodei.value.toLowerCase();
-    var encodei=document.morsecode.encodei.value.split("");
-    document.morsecode.encodeo.value="";
-    temp=''
-   
-    for (a=0; a<encodei.length; a++)
-    {
-        if (encodei[a]!=" ") 
-        {
-            if (window.charCodes[chars[a]]) 
-            {
-                document.morsecode.encodeo.value+=charCodes[chars[a]]+"    ";
-                temp+=chars[a]+"="+charCodes[chars[a]]+"\n";
-            }
-            else
-            temp+=chars[a]+"=(None)\n";
-        }
-        else temp+="\n";
-    }
-
-    document.morsecode.codebox.value+="\n\n\nEXPLANATION:\n\n"+temp
+function getTranslationURL(input) {
+    // if((input > 47 && input < 58) || (input > 64 && input < 91) ||  (input > 96 && input < 123))
+    // {
+        return serverURLtranseng + "?" + "text=" + input
+    // }
+    // else{
+    //     return serverURLtransmorse + "?" + "text=" + input
+    // }
+    
 }
 
-var charCodes=new Array(36); 
-charCodes["a"]=". _";
-charCodes["b"]="_ . . .";
-charCodes["c"]="_ . _ .";
-charCodes["d"]="_ . .";
-charCodes["e"]=".";
-charCodes["f"]=". . _ .";
-charCodes["g"]="_ _ .";
-charCodes["h"]=". . . .";
-charCodes["i"]=". .";
-charCodes["j"]=". _ _ _";
-charCodes["k"]="_ . _";
-charCodes["l"]=". _ . .";
-charCodes["m"]="_ _";
-charCodes["n"]="_ .";
-charCodes["o"]="_ _ _";
-charCodes["p"]=". _ _ .";
-charCodes["q"]="_ _ . _";
-charCodes["r"]=". _ .";
-charCodes["s"]=". . .";
-charCodes["t"]="_";
-charCodes["u"]=". . _";
-charCodes["v"]=". . . _";
-charCodes["w"]=". _ _";
-charCodes["x"]="_ . . _";
-charCodes["y"]="_ . _ _";
-charCodes["z"]="_ _ . .";
-charCodes["1"]=". _ _ _ _";
-charCodes["2"]=". . _ _ _";
-charCodes["3"]=". . . _ _";
-charCodes["4"]=". . . . _";
-charCodes["5"]=". . . . .";
-charCodes["6"]="_ . . . .";
-charCodes["7"]="_ _ . . .";
-charCodes["8"]="_ _ _ . .";
-charCodes["9"]="_ _ _ _ .";
-charCodes["0"]="_ _ _ _ _";
-var temp=''
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate_english.addEventListener("click", clickHandler)
+// btnTranslate_morse.addEventListener("click", clickHandler)
